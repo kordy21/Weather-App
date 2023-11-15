0ffeb33b6cfba7bf8d33177d42e0e6b49e3d5391
Kordy21 <m.abdelhamed941@gmail.com>
2023-11-15 10:27:13 +0200

    create app

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col min-h-screen bg-weather-primary">
+    <!-- <SiteNavigation /> -->
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+  // import SiteNavigation from './components/SiteNavigation.vue'
+</script>
+
+<style lang="scss" scoped>
+
+</style>
