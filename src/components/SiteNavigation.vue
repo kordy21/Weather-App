4dcf31b43dbfac5c6c7b7c376f74b4be3fa5a8c9
Kordy21 <m.abdelhamed941@gmail.com>
2023-11-16 21:13:46 +0200

    add BaseModal Component

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,5 +1,6 @@
 <template>
-  <header class="sticky top-0 bg-weather-primary shadow-lg">
+  <header
+   class="sticky top-0 bg-weather-primary shadow-lg">
     <nav  class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
       <RouterLink :to="{name:home}">
         <div class="flex items-center gap-3 ">
@@ -11,22 +12,54 @@
       </RouterLink>
 
       <div class="flex gap-3 flex-1 justify-end">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 hover:text-weather-secondary duration-150 cursor-pointer">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 hover:text-weather-secondary duration-150 cursor-pointer" @click="toggleModal">
         <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
       </svg>
       
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 hover:text-weather-secondary duration-150 cursor-pointer">
       <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
       </svg>
-
-
       </div>
+
+      <BaseModal :modalActive="modalActive" @close-model="toggleModal">
+        <div class="text-black">
+          <h1 class="text-2xl mb-1 font-medium">About:</h1>
+          <p class="mb-4">
+            The Local Weather allows you to track the current and future weather of cities of your choosing.
+          </p>
+          <h2 class="text-2xl font-medium">How it works:</h2>
+          <ol class="list-decimal list-inside mb-4">
+            <li>
+              search for yor city by entering the name into the search bar.
+            </li>
+            <li>
+              select a city within the results, this will take you the current weather for your selection.
+            </li>
+            <li>
+              Track the city by clicking on the "+" icon in the top right. this will save to view at a later time on the home page.
+            </li>
+          </ol>
+
+          <h2 class="text-2xl font-medium">Removing a city</h2>
+          <p>
+            If you no longer wish to track a city, simply select the city within the home page. At the bottom of the page, there will be am option to delete the city.
+          </p>
+        </div>
+      </BaseModal>
     </nav>
   </header>
 </template>
 
 <script setup>
+  import { ref } from "vue";
   import {RouterLink} from "vue-router"
+  import BaseModal from "./BaseModal.vue";
+
+  const modalActive = ref(null);
+  const toggleModal =()=>{
+    modalActive.value=!modalActive.value;
+    console.log(modalActive.value)
+  }
 </script>
 
 <style lang="scss" scoped>
